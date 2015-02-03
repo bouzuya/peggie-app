fb51f848e4b9f1e077c76b9176df853d20211e83
bouzuya <m@bouzuya.net>
2015-02-04 00:14:15 +0900

    add PegService#add test

--- test/services/peg-service.ts
+++ test/services/peg-service.ts
@@ -6,6 +6,7 @@ import PegService = require('../../app/scripts/services/peg-service');
 
 describe('PegService', function() {
   beforeEach(function() {
+    this.data1 = { peg: true, date: '2015-01-10', value: 1000 };
     this.service = new PegService();
   });
 
@@ -13,6 +14,16 @@ describe('PegService', function() {
     context('when empty', function() {
       it('returns []', function() {
         assert.deepEqual(this.service.toArray(), []);
+      });
+    });
+
+    context('when [data1]', function() {
+      beforeEach(function() {
+        this.service.add(this.data1);
+      });
+
+      it('returns [data1]', function() {
+        assert.deepEqual(this.service.toArray(), [this.data1]);
       });
     });
   });
