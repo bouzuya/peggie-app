1eb321f3a6bb3275c7ff1b8d7c703c43ab1eb73b
bouzuya <m@bouzuya.net>
2015-02-04 01:03:56 +0900

    add PegService#remove

--- app/scripts/services/peg-service.ts
+++ app/scripts/services/peg-service.ts
@@ -12,6 +12,12 @@ class PegService {
     this._items.splice(index, 0, item);
   }
 
+  remove(item: Item): void {
+    var index = this._items.indexOf(item);
+    if (index === -1) return;
+    this._items.splice(index, 1);
+  }
+
   toArray(): Array<Item> {
     return this._items;
   }
