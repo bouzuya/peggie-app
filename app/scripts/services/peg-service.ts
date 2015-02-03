419ba89f34aa214ccfec68a1e537159802625800
bouzuya <m@bouzuya.net>
2015-02-04 00:15:10 +0900

    add PegService#add fake

--- app/scripts/services/peg-service.ts
+++ app/scripts/services/peg-service.ts
@@ -1,6 +1,16 @@
 class PegService {
+  private _items: Array<{}>;
+
+  constructor() {
+    this._items = [];
+  }
+
+  add(item: {}): void {
+    this._items.push(item);
+  }
+
   toArray(): Array<{}> {
-    return [];
+    return this._items;
   }
 }
 
