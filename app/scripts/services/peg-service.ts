bc165403c4cdb24c9da6ba54f5287da5a6d4440e
bouzuya <m@bouzuya.net>
2015-02-04 00:27:23 +0900

    use Item

--- app/scripts/services/peg-service.ts
+++ app/scripts/services/peg-service.ts
@@ -1,16 +1,18 @@
+import Item = require('../models/item');
+
 class PegService {
-  private _items: Array<{ date: string }>;
+  private _items: Array<Item>;
 
   constructor() {
     this._items = [];
   }
 
-  add(item: { date: string }): void {
+  add(item: Item): void {
     var index = this._items.filter((i) => i.date > item.date).length;
     this._items.splice(index, 0, item);
   }
 
-  toArray(): Array<{ date: string }> {
+  toArray(): Array<Item> {
     return this._items;
   }
 }
