7d4a68248c2f5e7a632fd3b1568d2f21027b51e1
bouzuya <m@bouzuya.net>
2015-02-04 00:19:29 +0900

    add PegService#add test

--- test/services/peg-service.ts
+++ test/services/peg-service.ts
@@ -7,6 +7,8 @@ import PegService = require('../../app/scripts/services/peg-service');
 describe('PegService', function() {
   beforeEach(function() {
     this.data1 = { peg: true, date: '2015-01-10', value: 1000 };
+    this.data2 = { peg: true, date: '2015-01-05', value: 500 };
+    this.data3 = { peg: true, date: '2015-01-05', value: 100 };
     this.service = new PegService();
   });
 
@@ -24,6 +26,30 @@ describe('PegService', function() {
 
       it('returns [data1]', function() {
         assert.deepEqual(this.service.toArray(), [this.data1]);
+      });
+    });
+
+    context('when [data1, data2]', function() {
+      beforeEach(function() {
+        this.service.add(this.data1);
+        this.service.add(this.data2);
+      });
+
+      it('returns [data1, data2]', function() {
+        assert.deepEqual(this.service.toArray(), [this.data1, this.data2]);
+      });
+    });
+
+    context('when [data1, data2, data3]', function() {
+      beforeEach(function() {
+        this.service.add(this.data1);
+        this.service.add(this.data2);
+        this.service.add(this.data3);
+      });
+
+      it('returns [data1, data3, data2]', function() {
+        assert.deepEqual(this.service.toArray(), [
+          this.data1, this.data3, this.data2]);
       });
     });
   });
