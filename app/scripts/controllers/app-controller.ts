9b9a5547f5b3450f8db8a1b97c75118b5389398d
bouzuya <m@bouzuya.net>
2015-02-05 21:45:03 +0900

    add peg form

--- app/scripts/controllers/app-controller.ts
+++ app/scripts/controllers/app-controller.ts
@@ -1,9 +1,11 @@
+/// <reference path="../../../typings/moment/moment.d.ts" />
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 
 class AppController {
   static $inject = [];
 
   pegs: Array<{}>;
+  peg: {};
 
   constructor() {
     this.pegs = [
@@ -11,6 +13,17 @@ class AppController {
       { date: '2015-01-02', value: 2000 },
       { date: '2015-01-01', value: 1000 }
     ];
+    this._resetPeg();
+  }
+
+  add(): void {
+    var index = parseInt(this.peg.index, 10);
+    this.pegs.splice(index, 0, this.peg);
+    this._resetPeg();
+  }
+
+  _resetPeg(): void {
+    this.peg = { index: 0, date: moment().format('YYYY-MM-DD'), value: 0 };
   }
 }
 
