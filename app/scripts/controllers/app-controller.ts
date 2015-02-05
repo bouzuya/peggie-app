f52d8d14d98c9af4c004f65c044cfc655884a2bb
bouzuya <m@bouzuya.net>
2015-02-05 21:37:46 +0900

    display the pegs

--- app/scripts/controllers/app-controller.ts
+++ app/scripts/controllers/app-controller.ts
@@ -1,35 +1,16 @@
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 
-import HubotScriptService = require('../services/hubot-script-service');
-
 class AppController {
-  static $inject = [
-    '$scope',
-    'HubotScriptService'
-  ];
+  static $inject = [];
 
-  limit: number;
-  loaded: boolean;
-  query: string;
-  scripts: Array<{}>;
+  pegs: Array<{}>;
 
-  constructor(
-    $scope : ng.IScope,
-    HubotScriptService : HubotScriptService
-  ) {
-    this.limit = 10;
-    this.loaded = false;
-    this.scripts = [];
-
-    HubotScriptService.findAll().then((scripts) => {
-      this.loaded = true;
-      this.scripts = scripts;
-    });
-  }
-
-  more(): void {
-    var count = Math.min(this.limit + 10, this.scripts.length);
-    this.limit = count;
+  constructor() {
+    this.pegs = [
+      { date: '2015-01-03', value: 3000 },
+      { date: '2015-01-02', value: 2000 },
+      { date: '2015-01-01', value: 1000 }
+    ];
   }
 }
 
