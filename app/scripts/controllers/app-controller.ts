47e11d48e321327237f6816b959510a8175e9a33
bouzuya <m@bouzuya.net>
2015-02-05 22:00:34 +0900

    use PegStoreService

--- app/scripts/controllers/app-controller.ts
+++ app/scripts/controllers/app-controller.ts
@@ -1,24 +1,28 @@
 /// <reference path="../../../typings/moment/moment.d.ts" />
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 
-class AppController {
-  static $inject = [];
+import PegStoreService = require('../services/peg-store-service');
 
+class AppController {
+  static $inject = [
+    'PegStoreService'
+  ];
+
+  pegStoreService: PegStoreService;
   pegs: Array<{}>;
   peg: { index: string; };
 
-  constructor() {
-    this.pegs = [
-      { date: '2015-01-03', value: 3000 },
-      { date: '2015-01-02', value: 2000 },
-      { date: '2015-01-01', value: 1000 }
-    ];
+  constructor(
+    pegStoreService: PegStoreService
+  ) {
+    this.pegStoreService = pegStoreService;
+    this.pegs = pegStoreService.getAll();
     this._resetPeg();
   }
 
   add(): void {
     var index = parseInt(this.peg.index, 10);
-    this.pegs.splice(index, 0, this.peg);
+    this.pegStoreService.insert(index, this.peg);
     this._resetPeg();
   }
 
