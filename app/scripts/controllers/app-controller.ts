8432b7f3de6d8c3a18a126a65f3f92c7cb89f91f
bouzuya <m@bouzuya.net>
2015-02-06 13:32:52 +0900

    use pg-peg-form

--- app/scripts/controllers/app-controller.ts
+++ app/scripts/controllers/app-controller.ts
@@ -1,4 +1,3 @@
-/// <reference path="../../../typings/moment/moment.d.ts" />
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 
 import PegStoreService = require('../services/peg-store-service');
@@ -17,17 +16,6 @@ class AppController {
   ) {
     this.pegStoreService = pegStoreService;
     this.pegs = pegStoreService.getAll();
-    this._resetPeg();
-  }
-
-  add(): void {
-    var index = parseInt(this.peg.index, 10);
-    this.pegStoreService.insert(index, this.peg);
-    this._resetPeg();
-  }
-
-  _resetPeg(): void {
-    this.peg = { index: '0', date: moment().format('YYYY-MM-DD'), value: '0' };
   }
 }
 
