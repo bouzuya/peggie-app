76ac4f00ac2f6d648112f639ca1f0ba8c3e90d8e
bouzuya <m@bouzuya.net>
2015-02-07 13:30:51 +0900

    merge pg-peg-form to AppController

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -3,20 +3,16 @@
 import AppController = require('./controllers/app-controller');
 import DetailController = require('./controllers/detail-controller');
 import HubotScriptService = require('./services/hubot-script-service');
-import PegFormService = require('./services/peg-form-service');
 import PegStoreService = require('./services/peg-store-service');
 import PgPeg = require('./directives/pg-peg');
-import PgPegFormFactory = require('./directives/pg-peg-form');
 
 angular
 .module('app', [
   'ui.router'
 ])
 .service('HubotScriptService', HubotScriptService)
-.service('PegFormService', PegFormService)
 .service('PegStoreService', PegStoreService)
 .directive('pgPeg', () => new PgPeg())
-.directive('pgPegForm', PgPegFormFactory)
 .controller('AppController', AppController)
 .controller('DetailController', DetailController)
 .config([
--- app/scripts/controllers/app-controller.ts
+++ app/scripts/controllers/app-controller.ts
@@ -1,30 +1,48 @@
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../../typings/moment/moment.d.ts" />
 
-import PegFormService = require('../services/peg-form-service');
 import PegStoreService = require('../services/peg-store-service');
 
 class AppController {
   static $inject = [
-    'PegFormService',
     'PegStoreService'
   ];
 
-  pegFormService: PegFormService;
+  peg: {
+    peg: boolean;
+    index: number;
+    date: string;
+    value: number;
+  };
+
   pegStoreService: PegStoreService;
   pegs: Array<{}>;
-  peg: { index: string; };
 
   constructor(
-    pegFormService: PegFormService,
     pegStoreService: PegStoreService
   ) {
-    this.pegFormService = pegFormService;
+    this._reset();
     this.pegStoreService = pegStoreService;
     this.pegs = pegStoreService.getAll();
   }
 
-  formFor(index: number) {
-    this.pegFormService.setIndex(index);
+  formFor(index: number, peg: boolean) {
+    this.peg.index = index;
+    this.peg.peg = peg;
+  }
+
+  click(): void {
+    this.pegStoreService.insert(this.peg.index, this.peg);
+    this._reset();
+  }
+
+  _reset(): void {
+    this.peg = {
+      peg: true,
+      index: 0,
+      date: moment().format('YYYY-MM-DD'),
+      value: 0
+    };
   }
 }
 
