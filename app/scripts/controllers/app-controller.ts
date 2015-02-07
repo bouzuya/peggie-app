9c217e2754504387548b7b04cc8687ff4f1144a5
bouzuya <m@bouzuya.net>
2015-02-07 12:49:07 +0900

    use PegFormService in AppController

--- app/scripts/controllers/app-controller.ts
+++ app/scripts/controllers/app-controller.ts
@@ -1,21 +1,30 @@
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 
+import PegFormService = require('../services/peg-form-service');
 import PegStoreService = require('../services/peg-store-service');
 
 class AppController {
   static $inject = [
+    'PegFormService',
     'PegStoreService'
   ];
 
+  pegFormService: PegFormService;
   pegStoreService: PegStoreService;
   pegs: Array<{}>;
   peg: { index: string; };
 
   constructor(
+    pegFormService: PegFormService,
     pegStoreService: PegStoreService
   ) {
+    this.pegFormService = pegFormService;
     this.pegStoreService = pegStoreService;
     this.pegs = pegStoreService.getAll();
+  }
+
+  formFor(index: number) {
+    this.pegFormService.setIndex(index);
   }
 }
 
