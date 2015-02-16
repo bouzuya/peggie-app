3fe0f9dd150eba1e02100779e18b4a68c60b9ae0
bouzuya <m@bouzuya.net>
2015-02-16 14:36:35 +0900

    use PegType in PgPeg class

--- app/scripts/directives/pg-peg.ts
+++ app/scripts/directives/pg-peg.ts
@@ -1,9 +1,14 @@
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 
+import PegType = require('../models/peg-type');
+
 class PgPeg implements ng.IDirective {
   templateUrl: string = '../../views/directives/pg-peg.html';
   scope: any = {
     model: '='
+  };
+  link = function(scope) {
+    scope.PegType = PegType;
   };
 }
 
